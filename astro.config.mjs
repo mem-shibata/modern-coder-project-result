5c006f2665a55529f95ff150efb9dda84d3aa6b9
mc00393 <shibata.toshiya.mc00393@members.co.jp>
2025-09-01 17:49:43 +0900

    Added a setting to output CSS directly

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,13 @@ export default defineConfig({
   site: 'https://mem-shibata.github.io',
   base: './',
   output: 'static',
+  vite: {
+    build: {
+      cssMinify: false,
+      minify: false,
+    },
+    css: {
+      postcss: false,
+    },
+  },
 });
