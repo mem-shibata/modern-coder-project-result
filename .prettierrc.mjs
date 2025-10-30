572b5678ba658d22090845ca65713e70b8902ad0
mc00393 <shibata.toshiya.mc00393@members.co.jp>
2025-10-30 17:36:35 +0900

    第2回成果物

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,7 @@
-// .prettierrc.mjs
-/** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
-  // 基本的なPrettier設定
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
-  printWidth: 120,
+  semi: true, // セミコロンを文末に必ずつける
+  singleQuote: true, // 引用符を使用する際にシングルクォーテーションを使用する
+  tabWidth: 2, // タブの幅を2にする
+  trailingComma: 'es5', // 末尾のカンマを追加する
+  printWidth: 120, // 1行の最大文字数
 };
