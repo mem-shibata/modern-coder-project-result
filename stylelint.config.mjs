33925c97fda90368e3a69d6c7e0c3384393c0c8e
mc00393 <shibata.toshiya.mc00393@members.co.jp>
2026-03-11 16:52:07 +0900

    Stylelint設定の変更

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,5 +2,6 @@ export default {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   rules: {
     'declaration-property-value-no-unknown': null,
+    'selector-class-pattern': null,
   },
 };
